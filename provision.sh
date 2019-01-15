dc73d51e4936c56c5879e3a8143f25da956c0d2b
Bob Tantlinger <bob.tantlinger@gmail.com>
2019-01-14 19:17:57 -0500

    Update provision file

--- provision.sh
+++ provision.sh
@@ -1,17 +1,56 @@
 #!/bin/bash
 
-cat <<'EOF' | sudo tee /etc/nginx/sites-available/magento
+#Set the host name 
+SRV_NAME="magento-dev.local"
+
+#Set the php version.  May be 7.1 or 7.2
+PHP_VERSION="7.2"
+
+#Set Web root relative to /var/www
+WEB_ROOT="magento"
+
+
+
+# Update PHP version
+sudo update-alternatives --set php /usr/bin/php${PHP_VERSION}
+sudo systemctl stop php7.1-fpm.service #> /dev/null 2>&1 & 
+sudo systemctl stop php7.2-fpm.service #> /dev/null 2>&1 &
+sudo systemctl disable php7.1-fpm.service #> /dev/null 2>&1 & 
+sudo systemctl disable php7.2-fpm.service #> /dev/null 2>&1 &
+sudo systemctl enable php${PHP_VERSION}-fpm.service #> /dev/null 2>&1 &
+sudo systemctl start php${PHP_VERSION}-fpm.service #> /dev/null 2>&1 &
+
+
+cat <<EOF | sudo tee /etc/nginx/sites-available/magento
 upstream fastcgi_backend {
-	server unix:/run/php/php7.1-fpm.sock;
+	server unix:/run/php/php${PHP_VERSION}-fpm.sock;
 }
 
 server {
 	listen 80;
-	server_name magento-dev.local;
-	set $MAGE_ROOT /var/www/magento;
-	include /var/www/magento/nginx.conf.sample;
+	server_name ${SRV_NAME};
+	set \$MAGE_ROOT /var/www/${WEB_ROOT};
+	include /var/www/${WEB_ROOT}/nginx.conf.sample;
 }
 EOF
+
+cat <<PMA | sudo tee /etc/nginx/sites-available/phpmyadmin
+server {
+        listen 8001;
+        server_name _;
+        root /usr/share/phpmyadmin;
+        index index.php;
+        autoindex off;
+        charset UTF-8;
+        location / {
+                try_files \$uri \$uri/ /index.php\$is_args\$args;
+        }
+        location ~ \\.php$ {
+               include snippets/fastcgi-php.conf;
+               fastcgi_pass unix:/run/php/php${PHP_VERSION}-fpm.sock;
+        }
+}
+PMA
 
 sudo service nginx restart
 
